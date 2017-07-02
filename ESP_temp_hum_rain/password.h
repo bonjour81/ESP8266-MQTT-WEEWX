b3626ea1b9ec3a472c7fe1ba5b3b9337d727d303
bonjour81 <fstuyk@gmail.com>
2017-07-02 19:32:10 +0200

    Create password.h

---
+++ ESP_temp_hum_rain/password.h
@@ -0,0 +1,10 @@
+// credentials for wifi & mqtt
+
+const char* ssid = "<your wifi hotspot ssid>";
+const char* password = "<your wifi password>";
+
+#define AIO_SERVER      "<ip address of mosquitto server>"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "<mosquitto login>"
+#define AIO_KEY         "<mosquitto password>"
+
